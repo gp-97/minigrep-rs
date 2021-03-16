fce1b6888047f39c9a45678761d0ea4edd75dad6
Gaurav Patel <GauravPatel97@outlook.com>
2021-03-16 18:26:12 +0530

    Added number of matches printing

--- src/search.rs
+++ src/search.rs
@@ -2,10 +2,14 @@ use crate::config::Config;
 
 pub fn search(config: &Config, content: &String) {
     let query: &String = config.get_query();
-    println!("{}", query.as_str());
+    let mut instances = 0;
     for (count, line) in content.lines().enumerate() {
         if line.contains(query.as_str()) {
             println!("Line {}: {}", count + 1, line);
+            instances += 1;
         }
+    }
+    if instances == 0 {
+        println!("No match found");
     }
 }
